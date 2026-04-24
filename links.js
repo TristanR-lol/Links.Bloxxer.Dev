42d014a8cb2bb6542dc60b35159ecbdda37a29e1
TristanR-lol <tristan.raneroyay@proton.me>
2026-04-24 20:56:39 +0100

    Bug

--- links.js
+++ links.js
@@ -1,11 +1,11 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
-const supabase = createClient(process.env.ANON_KEY, process.env.PUBLISHABLE_KEY)
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabase = createClient(process.env.ANON_KEY, process.env.PUBLISHABLE_KEY);
 
 function AddItem(Link) {
-    const { data } = await supabase.from("shortlinks").select("id").order("id", { ascending: false }).limit(1)
-    const nextId = data[0].id + 1
-    const LID = Math.floor(Math.random() * 10000) + 1
-    await supabase.from("shortlinks").insert({ id: nextId, Link, LID }).then(console.log)
+    const { data } = await supabase.from("shortlinks").select("id").order("id", { ascending: false }).limit(1);
+    const nextId = data[0].id + 1;
+    const LID = Math.floor(Math.random() * 10000) + 1;
+    await supabase.from("shortlinks").insert({ id: nextId, Link, LID }).then(console.log);
 };
 
-AddItem("bloxxer.dev")
+AddItem("bloxxer.dev");
